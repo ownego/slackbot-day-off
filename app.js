b8dd1cf9d1ea6fad3af596e4364952140d657c56
vie02 <trankhactaia1@gmail.com>
2015-06-09 15:06:30 +0700

    fix 1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
-app.post('/hello', hellobot);
+app.post('/hello', hellobot.js);
  
 // error handler
 app.use(function (err, req, res, next) {
