7c49b7b4b0c67012af1217936bd4d4cad32b0c41
vie02 <trankhactaia1@gmail.com>
2015-06-09 15:08:32 +0700

    fix 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var hellobot = require('./hellobot.js');
+var hellobot = require('./hellobot');
  
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
-app.post('/hello', hellobot.js);
+app.post('/hello', hellobot);
  
 // error handler
 app.use(function (err, req, res, next) {
