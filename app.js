d76082aeb0a3ce29718beece31b29a921beb1e8f
vie02 <trankhactaia1@gmail.com>
2015-06-09 16:05:12 +0700

    rd

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var hellobot = require('./hellobot');
  
-// var app = express();
-// var port = process.env.PORT || 3000;
+var app = express();
+var port = process.env.PORT || 3000;
  
 // body parser middleware
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
-//app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 app.post('/hello', hellobot);
  
 // error handler
@@ -18,6 +18,6 @@ app.use(function (err, req, res, next) {
   res.status(400).send(err.message);
 });
  
-// app.listen(port, function () {
-//   console.log('Slack bot listening on port ' + port);
-// });
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
