de476676b9b3da448496c509be59064cf979355b
vie02 <trankhactaia1@gmail.com>
2015-06-10 23:11:28 +0700

    diceBot

--- hellobot.js
+++ hellobot.js
@@ -1,9 +1,10 @@
+
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
     text : 'Hello, ' + userName + '!'
-  };
- 
+  }
+
   // avoid infinite loop
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
